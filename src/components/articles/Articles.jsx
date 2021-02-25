059823ecf82e0a15f99a2cc815e55a273037fa55
lucasjesusone <lucasjesusone@gmail.com>
2021-02-25 02:14:03 -0400

    Criação Artigos

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IoWatchOutline } from "react-icons/io";
+import { VscWatch } from "react-icons/vsc";
 import { HiOutlineMicrophone } from "react-icons/hi";
 import { FiActivity } from "react-icons/fi";
 import { GrDiamond } from "react-icons/gr";
@@ -12,9 +12,9 @@ const Articles = () => {
       <div className="services">
         <div>
           <p className="icon">
-            <IoWatchOutline />
+            <VscWatch />
           </p>
-          <p classname="title">Spend time smart</p>
+          <p className="title">Spend time smart</p>
           <p className="text">Lorem ipsum sit amet lorem</p>
         </div>
 
@@ -22,15 +22,15 @@ const Articles = () => {
           <p className="icon">
             <HiOutlineMicrophone />
           </p>
-          <p classname="title">Spend time smart</p>
-          <p>Lorem ipsum sit amet lorem</p>
+          <p className="title">Spend time smart</p>
+          <p className="text">Lorem ipsum sit amet lorem</p>
         </div>
 
         <div>
           <p className="icon">
             <FiActivity />
           </p>
-          <p classname="title">Spend time smart</p>
+          <p className="title">Spend time smart</p>
           <p className="text">Lorem ipsum sit amet lorem</p>
         </div>
 
@@ -38,7 +38,7 @@ const Articles = () => {
           <p className="icon">
             <GrDiamond />
           </p>
-          <p classname="title">Spend time smart</p>
+          <p className="title">Spend time smart</p>
           <p className="text">Lorem ipsum sit amet lorem</p>
         </div>
       </div>
