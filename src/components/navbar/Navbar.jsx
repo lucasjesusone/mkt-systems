13fd7eb08da0063f2ddc8d4be794f282ec371733
Lucasjesusone <Lucasjesusone@gmail.com>
2021-02-25 13:55:04 -0400

    Mudanças na Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import "../../index.css";
 import "./nav.scss";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+  const onHandleClick = () => {
+    setClicked(!clicked);
+  };
   return (
     <navbar>
       <div className="content">
-        <div></div>
+        <div>
+         
+        </div>
         <ul>
           <li>HOME</li>
           <li>ARTICLES</li>
           <li>AUDIO</li>
         </ul>
+      </div>
+      <div className='menu-icon' onClick={onHandleClick}>
+        <i className={clicked ?'fas fa-times' : 'fas fa-bars'}></i>
       </div>
     </navbar>
   );
