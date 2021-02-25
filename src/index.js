d6eadb265088225fadc448d378797c593614e9f0
lucasjesusone <lucasjesusone@gmail.com>
2021-02-25 02:14:22 -0400

    Indexes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 import Navbar from './components/navbar/Navbar'
 import Header from './components/header/Header'
 import Articles from './components/articles/Articles'
+import Login from './components/Login/Login'
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
     <Header/>
     <Articles />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
